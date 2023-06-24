f2a77fc21fb881055bc7bfb674514c872e7ce5ab
1223Daniil <mashtallerdaniil@gmail.com>
2023-06-24 17:55:19 +0300

    init: online-Store

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,14 @@
+<script></script>
+<template>
+  <nav>
+    <div class="header">
+      <div class="headerContent">bgbgbbggbb</div>
+    </div>
+  </nav>
+</template>
+
+<style>
+.header {
+  margin: 50px, 0px, 50px, 0px;
+}
+</style>
