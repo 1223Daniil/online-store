5d6e35943968a6d4fce0b3a2fbede38e384c6d91
1223Daniil <mashtallerdaniil@gmail.com>
2023-06-29 21:24:59 +0300

    fixed image and add Main.Js

--- src/views/AppMain.vue
+++ src/views/AppMain.vue
@@ -23,6 +23,12 @@ export default {
       index: 0,
       active: [true, false, false],
       isHidden: false,
+      imageItem1: "./src/assets/girl1.jpg",
+      imageItem2: "./src/assets/girl2.jpg",
+      imageItem3: "./src/assets/girl3.jpg",
+      imageInfo1: "./src/assets/info1.png",
+      imageInfo2: "./src/assets/info2.png",
+      imageInfo3: "./src/assets/info3.png",
     };
   },
   methods: {
@@ -33,6 +39,9 @@ export default {
     },
     goShop() {
       this.$router.push({ name: "Shop" });
+    },
+    goBrand() {
+      this.$router.push({ name: "Brand" });
     },
   },
 
@@ -92,6 +101,74 @@ export default {
           class="girlImageOne"
           src="../assets/dmitriy-7DD6tfTKqS4-unsplash 1.png"
         />
+      </div>
+    </div>
+  </section>
+  <section class="mainContent">
+    <span class="collection_title">Новая коллекция</span>
+    <div class="collection_block">
+      <div class="collection_item" @click="goShop()">
+        <img :src="imageItem1" class="" alt="" />
+        <span class="card_title">Футболка USA</span>
+        <span class="card_price">$129</span>
+      </div>
+      <div class="collection_item" @click="goShop()">
+        <img :src="imageItem2" class="" alt="" width="350" height="478" />
+        <span class="card_title">Купальник Glow</span>
+        <span class="card_price">$129</span>
+      </div>
+      <div class="collection_item" @click="goShop()">
+        <img :src="imageItem3" class="" alt="" />
+        <span class="card_title">Свитшот Sweet Shot</span>
+        <span class="card_price">$129</span>
+      </div>
+    </div>
+    <button @click="goShop()" class="showShop_btn" id="btn">
+      Открыть магазин
+    </button>
+
+    <h2>Что для нас важно</h2>
+    <div class="info_block">
+      <div class="info_item">
+        <img class="info_img" :src="imageInfo1" />
+        <h3 class="info_title">Качество</h3>
+        <span class="info_text">
+          Наши профессионалы работают на лучшем оборудовании для пошива одежды
+          беспрецедентного качества
+        </span>
+      </div>
+      <div class="info_item">
+        <img class="info_img" :src="imageInfo2" />
+        <h3 class="info_title">Скорость</h3>
+        <span class="info_text">
+          Благодаря отлаженной системе в Womazing мы можем отшивать до 20-ти
+          единиц продукции в наших собственных цехах
+        </span>
+      </div>
+      <div class="info_item">
+        <img class="info_img" :src="imageInfo3" />
+        <h3 class="info_title">Ответственность</h3>
+        <span class="info_text">
+          Мы заботимся о людях и планете. Безотходное производство и комфортные
+          условия труда - все это Womazing
+        </span>
+      </div>
+    </div>
+
+    <div class="team_womazing">
+      <div class="team_block">
+        <img class="team_img" src="../assets/team.jpg" />
+        <div class="team_block_text">
+          <h3>Для каждой</h3>
+          <span class="team_text"
+            >Каждая девушка уникальна. Однако, мы схожи в миллионе мелочей.
+          </span>
+          <span class="team_text">
+            Womazing ищет эти мелочи и создает прекрасные вещи, которые выгодно
+            подчеркивают достоинства каждой девушки.
+          </span>
+          <a class="team_link" @click="goBrand()">Подробнее о бренде</a>
+        </div>
       </div>
     </div>
   </section>
@@ -195,5 +272,90 @@ export default {
 .fade-in {
   opacity: 1;
   transition: opacity 1s ease-in;
+}
+
+.mainContent {
+  margin-top: 130px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 130px;
+  text-align: left;
+}
+
+.collection_title {
+  margin-bottom: 92px;
+  color: #000;
+  font-size: 40px;
+  font-family: Raleway;
+}
+.collection_block {
+  display: flex;
+  align-items: center;
+  gap: 30px;
+}
+
+.collection_item {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+}
+
+.showShop_btn {
+  margin-top: 65px;
+  padding: 22px 50px;
+  text-align: center;
+  border: 1px solid #6e9c9f;
+  color: #6e9c9f;
+  font-size: 17px;
+  font-family: Raleway;
+  line-height: 138.9%;
+}
+
+.info_block {
+  display: flex;
+  align-items: center;
+  gap: 30px;
+}
+
+.info_item {
+  display: flex;
+  width: 350px;
+  flex-direction: column;
+  gap: 37px;
+}
+
+.info_text {
+  color: #000;
+  font-size: 17px;
+  font-family: Raleway;
+}
+
+.info_img {
+  width: 61px;
+  height: 61px;
+}
+
+.team_womazing {
+  display: flex;
+  flex-direction: column;
+  gap: 92px;
+}
+
+.team_block {
+  display: flex;
+  gap: 124px;
+}
+
+.team_block_text {
+  display: flex;
+  flex-direction: column;
+  gap: 28px;
+  width: 255px;
+  font-size: 17px;
+}
+
+.team_text {
+  font-family: "Raleway";
 }
 </style>
